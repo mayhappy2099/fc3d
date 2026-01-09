3f8e688bf9e7d38c0464b20652a9513963feac55
mayhappy2099 <hsny2pzzqz@privaterelay.appleid.com>
2026-01-09 19:55:45 +0800

    Add files via upload

---
+++ fc3d-github-pages/sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'fc3d-predictor-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// 安装Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// 激活Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有，返回缓存
+        if (response) {
+          return response;
+        }
+        // 否则发起网络请求
+        return fetch(event.request).then(response => {
+          // 检查是否是有效响应
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // 克隆响应
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
